4be1b71c812f6c02eeaaac37013c57349662a6e0
kalsing <gabrilel.kalsing@gmail.com>
2026-02-20 11:23:50 -0300

    feat: generate the token when the client logs in

--- controllers/LoginControl.js
+++ controllers/LoginControl.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import Post from "../models/Post.js";
 import bcrypt, { compare } from "bcrypt";
+import { generateToken } from "./JwtFunctions.js";
 
 class LoginControl {
 
@@ -22,11 +23,15 @@ class LoginControl {
     if (!hashCompare) {
       return res.status(400).send()
     }
+
+    const token = generateToken(user.id)
+
     return res.json({
       id: user.id,
       userId: user.id,  
       firstName: user.firstName,
       lastName: user.lastName,
+      token: token
     })
   }
 }
