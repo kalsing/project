81e3121933bf35d3fbefd2a01541a37cf83a25b9
kalsing <gabrilel.kalsing@gmail.com>
2026-02-23 11:23:31 -0300

    feat: added axios interceptor function to make token default on headers for any request

--- ReactProject/src/apis/api.js
+++ ReactProject/src/apis/api.js
@@ -4,4 +4,17 @@ const api = axios.create({
     baseURL: "http://localhost:3000"
 })
 
+
+api.interceptors.request.use((config) => {
+    const userData = localStorage.getItem("3chanUser");
+    if(userData){
+        const {token} = JSON.parse(userData);
+        if(token) {
+            config.headers ["Authorization"] = `Bearer ${token}`;
+
+        }
+    }
+    return config;
+});
+
 export default api;
